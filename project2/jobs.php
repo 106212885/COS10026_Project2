4ff28ba7926d94a2be8f97b129810d1b435ae0ef
TAN QIAO EN <106212885@student.swin.edu.au>
2025-11-15 13:57:23 +0800

    Changes dt dd all that on jobs.php

--- project2/jobs.php
+++ project2/jobs.php
@@ -3,59 +3,53 @@ include 'header.inc';
 include 'nav.inc';
 require_once('settings.php');
 
-// Connect to the MySQL database using settings.php variables
 $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
 
-// To check if connection failed
 if (!$conn) {
     echo "<p>Database connection failure</p>";
 } else {
-    // SQL query to select all job records from the jobs table
     $query = "SELECT * FROM jobs";
     $result = mysqli_query($conn, $query);
 
-    // If query returns results
     if ($result) {
         echo "<main class='job-list'>";
 
-        // Loop through each row (job record) in the jobs table
         while ($row = mysqli_fetch_assoc($result)) {
-            // Start a job section with the job's reference ID as the section ID
             echo "
-            <section id='" . $row['ref_id'] . "' class='job'>
-                <h2>" . $row['title'] . "</h2>";
+            <section id='" . $row['ref_id'] . "'>
+              <h2>" . $row['title'] . "</h2>
+              <dl class='job-details'>
+                <dt>
+                  <strong>" . $row['title'] . "</strong>";
 
-            // Display job image if the image field is not empty
             if ($row['image'] != "") {
                 echo "<img src='" . $row['image'] . "' alt='" . $row['title'] . "' class='job-image'>";
             }
 
-            // Display job details: reference ID, subtitle, salary, description, reporting manager
-            echo "
-                <p><strong>Reference ID:</strong> " . $row['ref_id'] . "</p>
-                <p><strong>Subtitle:</strong> " . $row['subtitle'] . "</p>
-                <p><strong>Salary range:</strong> " . $row['salary_range'] . "</p>
-                <p><strong>Position Description:</strong> " . $row['description'] . "</p>
-                <p><strong>Reports To:</strong> " . $row['reporting_manager'] . "</p>
-                
-                <!-- Expandable details section -->
-                <details>
-                    <summary>More Details</summary>
-                    <p>" . $row['details'] . "</p>";
+            echo "</dt>
+                <dd>
+                  <strong>Reference ID:</strong> " . $row['ref_id'] . "<br>
+                  <strong>Position Title:</strong> " . $row['title'] . "<br>
+                  <strong>Position Description:</strong> " . $row['description'] . "<br>
+                  <strong>Salary range:</strong> " . $row['salary_range'] . "<br>
+                  <a href='#" . $row['ref_id'] . "-details' class='button'>More Details</a><br>
+                  <details>
+                    <summary id='" . $row['ref_id'] . "-details'></summary>
+                    The title of the position to whom the successful applicant will report: " . $row['reporting_manager'] . "<br>
+                    " . $row['details'];
 
-            // Display reference link if available
             if ($row['references_link'] != "") {
-                echo "<a href='" . $row['references_link'] . "'>Reference Link</a>";
+                echo "<br><a href='" . $row['references_link'] . "'>Reference Link</a>";
             }
 
-            // Close details and section
             echo "</details>
+                </dd>
+              </dl>
             </section>";
         }
 
         echo "</main>";
     } else {
-        // If query failed, show error message
         echo "<p>Unable to retrieve jobs data.</p>";
     }
 
